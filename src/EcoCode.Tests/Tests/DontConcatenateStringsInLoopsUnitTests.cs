5c6d6e475c04c9b27c3ce9e3d1f3e4d763a6ed35
Vianney LAJOUMARD DE BELLABRE <vlajoumard@c2s.fr>
2024-03-19 19:50:28 +0100

    Ajout live tests

--- src/EcoCode.Tests/Tests/DontConcatenateStringsInLoopsUnitTests.cs
+++ src/EcoCode.Tests/Tests/DontConcatenateStringsInLoopsUnitTests.cs
@@ -22,23 +22,27 @@ public class DontConcatenateStringsInLoopsUnitTests
                 for (int i = 0; i < 10; i++)
                     [|s0 += i;|]
                 for (int i = 0; i < 10; i++)
-                    s0 = i.ToString();
+                    s0 = i.ToString(provider: null);
+                Console.WriteLine(s0);
 
                 for (int i = 0; i < 10; i++)
                     [|s1 += i;|]
                 for (int i = 0; i < 10; i++)
-                    s1 = i.ToString();
+                    s1 = i.ToString(provider: null);
+                Console.WriteLine(s1);
 
                 for (int i = 0; i < 10; i++)
                     [|s2 += i;|]
                 for (int i = 0; i < 10; i++)
-                    s2 = i.ToString();
+                    s2 = i.ToString(provider: null);
+                Console.WriteLine(s2);
 
                 string s3 = string.Empty;
                 for (int i = 0; i < 10; i++)
                     [|s3 += i;|]
                 for (int i = 0; i < 10; i++)
-                    s3 = i.ToString();
+                    s3 = i.ToString(provider: null);
+                Console.WriteLine(s3);
             }
         }
         """).ConfigureAwait(false);
@@ -55,21 +59,26 @@ public class DontConcatenateStringsInLoopsUnitTests
                 string s3 = string.Empty;
                 for (int i = 0; i < 10; i++)
                 {
-                    s0 = i.ToString();
+                    s0 = i.ToString(provider: null);
                     [|s0 += i;|]
+                    Console.WriteLine(s0);
 
-                    s1 = i.ToString();
+                    s1 = i.ToString(provider: null);
                     [|s1 += i;|]
+                    Console.WriteLine(s1);
 
-                    s2 = i.ToString();
+                    s2 = i.ToString(provider: null);
                     [|s2 += i;|]
+                    Console.WriteLine(s2);
 
-                    s3 = i.ToString();
+                    s3 = i.ToString(provider: null);
                     [|s3 += i;|]
+                    Console.WriteLine(s3);
 
                     string s4;
-                    s4 = i.ToString();
+                    s4 = i.ToString(provider: null);
                     s4 += i;
+                    Console.WriteLine(s4);
                 }
             }
         }
