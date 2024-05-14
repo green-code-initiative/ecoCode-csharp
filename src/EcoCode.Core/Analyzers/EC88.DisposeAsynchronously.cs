b258084aeeefff2965937f833ccc54fc926b7569
Vianney de Bellabre <vdebellabre@c2s.fr>
2024-05-14 09:10:52 +0200

    Fix

--- src/EcoCode.Core/Analyzers/EC88.DisposeAsynchronously.cs
+++ src/EcoCode.Core/Analyzers/EC88.DisposeAsynchronously.cs
@@ -4,7 +4,8 @@
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public sealed class DisposeAsynchronously : DiagnosticAnalyzer
 {
-    private static readonly ImmutableArray<SyntaxKind> SyntaxKinds = [SyntaxKind.UsingStatement];
+    private static readonly ImmutableArray<SyntaxKind> UsingStatementKinds = [SyntaxKind.UsingStatement];
+    private static readonly ImmutableArray<SyntaxKind> UsingDeclarationKinds = [SyntaxKind.LocalDeclarationStatement];
 
     /// <summary>The diagnostic descriptor.</summary>
     public static DiagnosticDescriptor Descriptor { get; } = new(
@@ -26,23 +27,58 @@ public sealed class DisposeAsynchronously : DiagnosticAnalyzer
     {
         context.EnableConcurrentExecution();
         context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
-        context.RegisterSyntaxNodeAction(static context => AnalyzeUsingStatement(context), SyntaxKinds);
+        context.RegisterSyntaxNodeAction(static context => AnalyzeUsingStatement(context), UsingStatementKinds);
+        context.RegisterSyntaxNodeAction(static context => AnalyzeUsingDeclaration(context), UsingDeclarationKinds);
     }
 
     private static void AnalyzeUsingStatement(SyntaxNodeAnalysisContext context)
     {
-        var usingStatement = (UsingStatementSyntax)context.Node;
-        if (usingStatement.Declaration is not { } declaration) return;
-
-        var typeInfo = context.SemanticModel.GetTypeInfo(declaration.Type);
-
-        if (typeInfo.Type is INamedTypeSymbol namedTypeSymbol &&
-            context.Compilation.GetTypeByMetadataName(typeof(IAsyncDisposable).FullName) is { } asyncDisposableType &&
-            namedTypeSymbol.AllInterfaces.Contains(asyncDisposableType) &&
-            context.SemanticModel.GetEnclosingSymbol(context.Node.SpanStart) is IMethodSymbol methodSymbol &&
-            methodSymbol.IsAsync)
+        var statement = (UsingStatementSyntax)context.Node;
+        if (!statement.AwaitKeyword.IsKind(SyntaxKind.AwaitKeyword) &&
+            statement.Declaration is { } declaration &&
+            CanBeDisposedAsynchronously(context, declaration.Variables) &&
+            IsContainedInAsyncMethod(statement))
         {
-            context.ReportDiagnostic(Diagnostic.Create(Descriptor, usingStatement.GetLocation()));
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, statement.UsingKeyword.GetLocation()));
         }
+    }
+
+    private static void AnalyzeUsingDeclaration(SyntaxNodeAnalysisContext context)
+    {
+        var statement = (LocalDeclarationStatementSyntax)context.Node;
+        if (statement.UsingKeyword.IsKind(SyntaxKind.UsingKeyword) &&
+            !statement.AwaitKeyword.IsKind(SyntaxKind.AwaitKeyword) &&
+            CanBeDisposedAsynchronously(context, statement.Declaration.Variables) &&
+            IsContainedInAsyncMethod(statement))
+        {
+            context.ReportDiagnostic(Diagnostic.Create(Descriptor, statement.UsingKeyword.GetLocation()));
+        }
+    }
+
+    private static bool CanBeDisposedAsynchronously(SyntaxNodeAnalysisContext context, SeparatedSyntaxList<VariableDeclaratorSyntax> variables) =>
+        variables.Count == 1 && variables[0].Initializer?.Value is { } expr && // Don't handle multiple variables declarations
+        context.SemanticModel.GetTypeInfo(expr, context.CancellationToken).Type is INamedTypeSymbol namedTypeSymbol &&
+        context.Compilation.GetTypeByMetadataName(typeof(IAsyncDisposable).FullName) is { } asyncDisposableType &&
+        namedTypeSymbol.AllInterfaces.Contains(asyncDisposableType, SymbolEqualityComparer.Default);
+
+    private static bool IsContainedInAsyncMethod(StatementSyntax statement)
+    {
+        for (var node = statement.Parent; node is not null; node = node.Parent)
+        {
+            switch (node)
+            {
+                case MemberDeclarationSyntax:
+                    return node is MethodDeclarationSyntax methodDeclaration && methodDeclaration.Modifiers.Contains(SyntaxKind.AsyncKeyword);
+                case LocalFunctionStatementSyntax localFunction:
+                    return localFunction.Modifiers.Contains(SyntaxKind.AsyncKeyword);
+                case LambdaExpressionSyntax lambdaExpression:
+                    return lambdaExpression.Modifiers.Contains(SyntaxKind.AsyncKeyword);
+                case AnonymousMethodExpressionSyntax anonymousMethod:
+                    return anonymousMethod.Modifiers.Contains(SyntaxKind.AsyncKeyword);
+                case LockStatementSyntax:
+                    return false;
+            }
+        }
+        return false;
     }
 }
