1937235f241f30f8e8a3913c5bb59ce89aa94380
jraillard <dangrall49@gmail.com>
2024-05-30 12:38:31 +0200

    feat(core) : Ajout de spectre

--- src/EcoCode.Tool/Program.cs
+++ src/EcoCode.Tool/Program.cs
@@ -1,12 +1,47 @@
 ﻿using Microsoft.CodeAnalysis.MSBuild;
+using Spectre.Console;
 
-const string targetPath = @"C:\ecocode\ecoCode-csharp-test-project\ecoCode-csharp-test-project.sln";
+namespace EcoCode.Tool;
 
-using (var workspace = MSBuildWorkspace.Create())
+/// <summary>
+/// EcoCode.Tool Main program
+/// </summary>
+public static class Program
 {
-    var sln = await workspace.OpenSolutionAsync(targetPath).ConfigureAwait(false);
-    foreach (var project in sln.Projects)
-        Console.WriteLine(project.AssemblyName);
-}
+    /// <summary>
+    /// EcoCode.tool Entry point
+    /// </summary>
+    /// <param name="args">CLI arguments</param>
+    public static async Task Main(string[] args)
+    {
+        const string targetPath = @"C:\ecocode\ecoCode-csharp-test-project\ecoCode-csharp-test-project.sln";
 
-Console.ReadKey();
+        AnsiConsole.Write(new Rule("[bold yellow]EcoCode.Tool[/]").Centered());
+
+        using (var workspace = MSBuildWorkspace.Create())
+        {
+            var solution = await workspace.OpenSolutionAsync(targetPath).ConfigureAwait(false);
+            foreach (var project in solution.Projects)
+            {
+                foreach (var document in project.Documents)
+                {
+                    var semanticModel = await document.GetSemanticModelAsync().ConfigureAwait(false);
+
+                    if (semanticModel is null)
+                    {
+                        AnsiConsole.Write(new Markup($"[bold red]{document.FilePath}: SemanticModel cannot be generated.[/]"));
+                        continue;
+                    }
+                    var diagnostics = semanticModel.GetDiagnostics();
+
+                    foreach (var diagnostic in diagnostics)
+                    {
+                        AnsiConsole.Write(new Markup($"[blue]{document.FilePath}: {diagnostic}.[/]"));
+                    }
+                }
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
