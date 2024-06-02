dc2573c4712d62823e45921f91ed61db88f28847
Vianney de Bellabre <vdebellabre@c2s.fr>
2024-06-02 05:45:09 +0200

    Fix

--- src/EcoCode.ToolNetFramework/Program.cs
+++ src/EcoCode.ToolNetFramework/Program.cs
@@ -24,20 +24,20 @@ internal class Program
             return;
         }
 
-        var instance = MSBuildLocator.QueryVisualStudioInstances().FirstOrDefault();
-        if (instance is null)
+        if (MSBuildLocator.QueryVisualStudioInstances().FirstOrDefault() is not { } instance)
         {
             Console.WriteLine($"No MSBuild instance was found, exiting.");
             return;
         }
-
         Console.WriteLine($"Using MSBuild at '{instance.MSBuildPath}' to load projects.");
         MSBuildLocator.RegisterInstance(instance);
 
         using var workspace = MSBuildWorkspace.Create();
         workspace.WorkspaceFailed += (sender, e) => Console.WriteLine(e.Diagnostic.Message);
 
-        if (Path.GetExtension(path) == ".sln")
+        string extension = Path.GetExtension(path);
+        if (string.Equals(extension, ".sln", StringComparison.OrdinalIgnoreCase) ||
+            string.Equals(extension, ".slnf", StringComparison.OrdinalIgnoreCase))
         {
             var solution = await workspace.OpenSolutionAsync(path);
             if (solution is null)
@@ -46,39 +46,39 @@ internal class Program
             }
             else
             {
+                var analyzers = LoadAnalyzers();
                 foreach (var project in solution.Projects)
-                    await AnalyzeProject(project);
+                    await AnalyzeProject(project, analyzers);
             }
         }
-        else if (Path.GetExtension(path) == ".csproj")
+        else if (string.Equals(extension, ".csproj", StringComparison.OrdinalIgnoreCase))
         {
             var project = await workspace.OpenProjectAsync(path);
             if (project is null)
                 Console.WriteLine("Cannot load the provided project");
             else
-                await AnalyzeProject(project);
+                await AnalyzeProject(project, LoadAnalyzers());
         }
         else
         {
-            Console.WriteLine("Please provide a valid .sln or .csproj file.");
+            Console.WriteLine("Please provide a valid .sln, .slnf or .csproj file.");
         }
 
         Console.WriteLine("Press a key to exit..");
         _ = Console.ReadKey();
     }
 
-    private static async Task AnalyzeProject(Project project)
+    private static async Task AnalyzeProject(Project project, ImmutableArray<DiagnosticAnalyzer> analyzers)
     {
         Console.WriteLine($"Analyzing project {project.Name}...");
 
-        var compilation = await project.GetCompilationAsync();
-        var analyzers = LoadAnalyzers();
+        if (await project.GetCompilationAsync() is not { } compilation)
+        {
+            Console.WriteLine($"Unable to load the project {project.Name} compilation, skipping");
+            return;
+        }
 
-        var compilationWithAnalyzers = compilation!.WithAnalyzers(analyzers);
-
-        var diagnostics = await compilationWithAnalyzers.GetAnalyzerDiagnosticsAsync();
-
-        foreach (var diagnostic in diagnostics)
+        foreach (var diagnostic in await compilation!.WithAnalyzers(analyzers).GetAnalyzerDiagnosticsAsync())
             Console.WriteLine(diagnostic.ToString());
 
         Console.WriteLine($"Analysis complete for project {project.Name}");
@@ -86,13 +86,12 @@ internal class Program
 
     private static ImmutableArray<DiagnosticAnalyzer> LoadAnalyzers()
     {
-        // Load your custom analyzers here.
-        // For example, if your analyzers are in a separate assembly, you can use reflection to load them.
-        var analyzers = ImmutableArray.Create<DiagnosticAnalyzer>(
-            new AvoidAsyncVoidMethods(),
-            new UseListIndexer()
-        );
-
-        return analyzers;
+        var analyzers = ImmutableArray.CreateBuilder<DiagnosticAnalyzer>(64);
+        foreach (var type in typeof(DontCallFunctionsInLoopConditions).Assembly.GetTypes())
+        {
+            if (type.IsSealed && type.IsSubclassOf(typeof(DiagnosticAnalyzer)))
+                analyzers.Add((DiagnosticAnalyzer)Activator.CreateInstance(type));
+        }
+        return analyzers.ToImmutableArray();
     }
 }
