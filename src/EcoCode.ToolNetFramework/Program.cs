5760f02d5c021424aaed69795eee582012974105
Vianney de Bellabre <vdebellabre@c2s.fr>
2024-06-08 15:09:47 +0200

    Fix

--- src/EcoCode.ToolNetFramework/Program.cs
+++ src/EcoCode.ToolNetFramework/Program.cs
@@ -15,6 +15,14 @@ internal class Program
     private const string SolutionDir = @"C:\Users\vlajoumard\source\ecoCode-csharp-test-project";
     private const string SolutionPath = @$"{SolutionDir}\ecoCode-csharp-test-project.sln";
 
+    // -s or --source: .sln, .slnf or .csproj in absolute/relative path. Takes the one in the same directory if not specified, error if there are several.
+    // -o or --output: .htm, .html, .json, csv or .txt in absolute/relative path. In the same directory as the source if the path is not specified, with the same name if not specified.
+    // -a or --analyzers to specify the analyzers to use. Default is All. Ex: -a "EC72;EC75;EC81"
+    // -i or --ignore to specify the analyzers to ignore. Default is Empty. Ex: -i "EC84;EC85;EC86". Has priority over -a.
+    // -r or --severity to specify the minimum severity of the analyzers to use. Can be All, Info, Warning or Error. Default is All.
+    // -v or --verbosity to customize information display about the analysis. Can be None, Normal or Detailed. Default is Normal.
+    // -h or --help to display the help message.
+
     public static async Task Main(string[] args)
     {
         string path = args.Length == 0 ? SolutionPath : args[0];
